691b997ddd0a5d064aca49139a411f518c4e758d
Chris Faulkner <thefaulkner@gmail.com>
2012-06-28 00:19:56 -0700

    Add sf.funcheap.com.

---
+++ sf.funcheap.com.js
@@ -0,0 +1,11 @@
+// stop crashing my browser, ass.
+$('iframe').remove();
+$('#box').remove();
+$('#fbComments').remove();
+$('#sidebar').remove();
+$('#logo').remove();
+$('#head').remove();
+$('#navbar').remove();
+$('#front-list').remove();
+$('#front-popular').remove();
+$('#footer').remove();
