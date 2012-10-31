1752dbb37a28ca92c446091955dd91cf58edc993
Chris Faulkner <thefaulkner@gmail.com>
2012-10-30 23:33:39 -0700

    Slap a click event on the github pull request arrow to flip the merge direction.

--- github.com.js
+++ github.com.js
@@ -2,3 +2,11 @@ if (window.location.pathname === '/search') {
     /* You pushed an empty repo?  Cool story, bro. */
     $('.result:contains("    0 Bytes")').hide();
 }
+
+/* click the pull request arrow to flip the merge direction. */
+var $pull_arrow = $('.mini-icon.mini-icon-arr-left.action-indicator');
+if ($pull_arrow.length) {
+    $pull_arrow.live('click', function(e) {
+        window.location.href = window.location.pathname.replace(/(.*)\/(.*)\.\.\.(.*)/, "$1/$3...$2");
+    });
+}
